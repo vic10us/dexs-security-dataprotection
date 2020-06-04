c394d423caa8268a44662daf2a9c4d9529df8948
@vic10us <dev@vic10.us>
2020-06-03 20:04:18 -0400

    feat(firstRelease): The first major release

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -81,10 +81,4 @@ namespace DEXS.Security.DataProtection.Tests
         }
 
     }
-
-    public class TestClass
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
 }
